ab91c2787424f6d400347d994d4053d155fb5967
Chaiprasit Khamsi <chaiprasit.kh.61@ubu.ac.th>
2020-09-30 10:06:18 +0700

    Updates

---
+++ precache-manifest.548b6252083e6792b2c7d56638dace33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c8574671cd3d740e3f06cb8d69d767",
+    "url": "/commath-project/index.html"
+  },
+  {
+    "revision": "5234380842796bf63dcc",
+    "url": "/commath-project/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/commath-project/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/commath-project/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5234380842796bf63dcc",
+    "url": "/commath-project/static/js/main.013f94c6.chunk.js"
+  },
+  {
+    "revision": "32f60014b64bfb1213fb",
+    "url": "/commath-project/static/js/runtime-main.7635a7cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/commath-project/static/media/logo.5d5d9eef.svg"
+  }
+]);
