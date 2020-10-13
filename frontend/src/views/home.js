2b88dbf6141d028eda2fd67e5e512857c855d51b
chaiprasit <chaiprasit.kh.61@ubu.ac.th>
2020-10-13 22:58:11 +0700

    newcss

--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -13,28 +13,33 @@ function Home() {
   }
 
   return (
-    <div>
-        <Button onClick={()=>{nextPage("BasicComputing")}} color="danger">
-            <h5>1.Basic Computing</h5>
-        </Button><br/>
-        <Button onClick={()=>{nextPage("LinearEquations")}} color="danger">
-            <h5>2.Linear Equations</h5>
-        </Button><br/>
-        <Button onClick={()=>{nextPage("Interpolation")}} color="danger">
-            <h5>3.Interpolation	</h5>
-        </Button><br/>
-        <Button onClick={()=>{nextPage("Differentiation")}} color="danger">
-            <h5>4.Differentiation</h5>
-        </Button><br/>
-        <Button onClick={()=>{nextPage("Integration")}} color="danger">
-            <h5>5.Integration	</h5>
-        </Button><br/>
-        <Button onClick={()=>{nextPage("Rootfinding")}} color="danger">
-            <h5>6.Root-finding</h5>
-        </Button><br/>
-        
+    <div class="center">
+        <div class="text-center">
+        <Button onClick={()=>{nextPage("BasicComputing")}} color="danger"><h5>1.Basic Computing</h5></Button><br/>
+        </div>
+        <br/>
+        <div class="text-center">
+        <Button onClick={()=>{nextPage("LinearEquations")}} color="danger"><h5>2.Linear Equations</h5></Button><br/>
+        </div>
+        <br/>
+        <div class="text-center">
+        <Button onClick={()=>{nextPage("Interpolation")}} color="danger"><h5>3.Interpolation</h5></Button><br/>
+        </div>
+        <br/>
+        <div class="text-center">
+        <Button onClick={()=>{nextPage("Differentiation")}} color="danger"><h5>4.Differentiation</h5></Button><br/>
+        </div>
+        <br/>
+        <div class="text-center">
+        <Button onClick={()=>{nextPage("Integration")}} color="danger"><h5>5.Integration</h5></Button><br/>
+        </div>
+        <br/>
+        <div class="text-center">
+        <Button onClick={()=>{nextPage("Rootfinding")}} color="danger"><h5>6.Root-finding</h5></Button><br/>
+        </div>
     </div>
   );
 }
+
 
 export default Home;
